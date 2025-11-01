6d46e99462184c1fee927a410e762ce9dc8fad12
anouar baoutoul <anouar.baoutoul@emsi-edu.ma>
2025-11-01 21:56:37 +0100

    Create guessinggame.sh for file counting game
    Added a guessing game script that prompts the user to guess the number of files in the current directory.

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
+
+# Function to count files in current directory
+count_files() {
+    ls -1 | wc -l
+}
+
+# Main game loop
+main() {
+    num_files=$(count_files)
+    
+    echo "How many files are in the current directory?"
+    read -p "Enter your guess: " guess
+    
+    while [[ $guess -ne $num_files ]]; do
+        if [[ $guess -lt $num_files ]]; then
+            echo "Your guess is too low. Try again."
+        else
+            echo "Your guess is too high. Try again."
+        fi
+        read -p "Enter your guess: " guess
+    done
+    
+    echo "Congratulations! You got it right!"
+}
+
+main
 
